dcf596d83c7b5107d6dc188b2495e6f365b6fa3e
Jason-Ng00 <e0407783@u.nus.edu>
2021-10-16 15:44:02 +0800

    redirect homepage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,17 @@
-// exports.createPages = async ({ actions }) => {
-//     const { createRedirect } = actions
+exports.createPages = async ({ actions }) => {
+    const { createRedirect } = actions
   
-//     createRedirect({
-//       fromPath: '/',
-//       toPath: '/introduction',
-//       isPermanent: true,
-//       redirectInBrowser: true,
-//    })
-//   }
+    createRedirect({
+      fromPath: '/',
+      toPath: '/introduction',
+      isPermanent: true,
+      redirectInBrowser: true,
+   })
+  }
 
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-
 // You can delete this file if you're not using it
